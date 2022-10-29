4198ffb8a67a289096c2b987404310f69bf06998
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-10-30 01:26:35 +0900

    .
    삽질실패ㅜㅜ

--- MYMP/app/src/main/java/com/kmu/mymp/ThirdActivity.kt
+++ MYMP/app/src/main/java/com/kmu/mymp/ThirdActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.kmu.mymp.databinding.ActivitySecondBinding
 import com.kmu.mymp.databinding.ActivityThirdBinding
 
 class ThirdActivity : AppCompatActivity() {
